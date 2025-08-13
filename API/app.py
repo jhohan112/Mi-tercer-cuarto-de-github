c2a648fddb468c5cd04fc9a73ac7b003ec06ec83
jhohan112 <79213639+jhohan112@users.noreply.github.com>
2025-08-13 09:08:27 -0500

    Update app.py

--- API/app.py
+++ API/app.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/familia")
 def get_familia():
-    rows = ["Amin", "Marce", "Miranda"]
+    rows = ["Jhohan", "Yuly", "Francy"]
     return rows
 
 @app.get("/superheroesDC")
